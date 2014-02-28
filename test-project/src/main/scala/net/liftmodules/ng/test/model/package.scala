6ad46e58ee1a7325ebac9539e9f5c114fb7af713
Joe Barnes <barnesjd@gmail.com>
2014-02-27 20:19:39 -0600

    Finished the second test

---
+++ test-project/src/main/scala/net/liftmodules/ng/test/model/package.scala
@@ -0,0 +1,8 @@
+package net.liftmodules.ng
+package test
+
+import Angular.NgModel
+
+package object model {
+  case class Test2Obj(str1:String, str2:String) extends NgModel
+}
--- test-project/src/main/scala/net/liftmodules/ng/test/snippet/Snips.scala
+++ test-project/src/main/scala/net/liftmodules/ng/test/snippet/Snips.scala
@@ -3,6 +3,7 @@ package test.snippet
 
 import Angular._
 import scala.xml.NodeSeq
+import test.model.Test2Obj
 
 import net.liftweb.common.{Loggable, Empty, Full}
 
@@ -18,5 +19,14 @@ object Snips extends Loggable {
   }).jsonCall("call2", (str:String) => {
       logger.info(s"call2($str) received on server.")
       Full(s"FromServer $str")
+  })))
+
+  def renderSingle(xhtml:NodeSeq) = renderIfNotAlreadyDefined(angular.module("SnipServices2")
+    .factory("snipServices2", jsObjFactory()
+
+    .jsonCall("call", (obj:Test2Obj) => {
+      import obj._
+      logger.info(s"call($obj) received on server.")
+      Full(Test2Obj(s"FromServer $str1", s"FromServer $str2"))
   })))
 }
