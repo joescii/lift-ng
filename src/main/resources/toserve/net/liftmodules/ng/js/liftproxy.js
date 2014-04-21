8fa9533b119661cabef35fb09498f882412eae95
Joe Barnes <barnesjd@gmail.com>
2014-04-20 21:52:26 -0500

    The LAFuture works for the no arg case

--- src/main/resources/toserve/net/liftmodules/ng/js/liftproxy.js
+++ src/main/resources/toserve/net/liftmodules/ng/js/liftproxy.js
@@ -2,17 +2,6 @@ angular
   .module('zen.lift.proxy', [])
   .factory('liftProxy', ['$http', '$q', '$rootScope', function ($http, $q, $rootScope) {
     return function (requestData) {
-      var onSuccess = function (response) {
-        var data = response.data, returnData;
-        if (data.success) {
-          if (data.data) {
-            returnData = data.data;
-          }
-        } else {
-          return $q.reject(data.msg)
-        }
-        return returnData;
-      };
       var random = function() {
         var text = "";
         var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -26,16 +15,38 @@ angular
       var id = random();
       var req = requestData.name+'='+encodeURIComponent(JSON.stringify({id:id, data:requestData.data}));
 
-      var cleanup = $rootScope.$on('lift-ng-future', function(e, response){
-        console.log('Response!');
-        console.log(response);
-        cleanup();
-      });
+      var returnQ = function(response) {
+        var q = $q.defer();
+        var data = response.data, returnData;
+        var responseToQ = function(data) {
+          if (data.success) {
+            if (data.data) {
+              q.resolve(data.data);
+            }
+            else {
+              q.resolve();
+            }
+          } else {
+            q.reject(data.msg)
+          }
+        };
+        if(data.future) {
+          var cleanup = $rootScope.$on('lift-ng-future', function(e, response){
+            console.log('Response!');
+            console.log(response);
+            responseToQ(response);
+            cleanup();
+          });
+        } else {
+          responseToQ(data)
+        }
+        return q.promise;
+      };
 
       return $http.post('/ajax_request/' + lift_page + '/', req, {
         headers : {
           'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         }
-      }).then(onSuccess);
+      }).then(returnQ);
     };
   }]);
