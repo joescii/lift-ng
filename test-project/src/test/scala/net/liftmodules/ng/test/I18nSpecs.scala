6613a7778387dbc4e73e5f38dfba660a036deb04
Joe Barnes <barnesjd@gmail.com>
2014-04-29 09:28:55 -0500

    Automated the non-english i18n test

--- test-project/src/test/scala/net/liftmodules/ng/test/I18nSpecs.scala
+++ test-project/src/test/scala/net/liftmodules/ng/test/I18nSpecs.scala
@@ -13,4 +13,18 @@ class I18nSpecs extends BaseSpec{
   "The 1-arg value" should "be 'Goodbye, Cruel World!'" in {
     id("params").element.text should be ("Goodbye, Cruel World!")
   }
+
+
+  "The i18n-non-english page" should "load" in {
+    go to s"$index/i18n-non-english"
+    eventually { pageTitle should be ("App: i18n - Non-English") }
+  }
+
+  "The no-arg value" should "be the correct Chinese" in {
+    id("no-params").element.text should be ("\u4F60\u597D")
+  }
+
+  "The 1-arg value" should "be the correct Hebrew" in {
+    id("params").element.text should be ("!Cruel World ,\u05E9\u05DC\u05D5\u05DD")
+  }
 }
