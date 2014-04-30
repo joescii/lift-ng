111334b14ad9ee2f2e460305351eab5794e61401
Joe Barnes <barnesjd@gmail.com>
2014-04-30 12:52:34 -0500

    Automated testing of the 2-bundle test page

--- test-project/src/test/scala/net/liftmodules/ng/test/I18nSpecs.scala
+++ test-project/src/test/scala/net/liftmodules/ng/test/I18nSpecs.scala
@@ -6,11 +6,11 @@ class I18nSpecs extends BaseSpec{
     eventually { pageTitle should be ("App: i18n - 1 name") }
   }
 
-  "The no-arg value" should "be 'Howdy!'" in {
+  "The no-arg value on the i18n-1name page" should "be 'Howdy!'" in {
     id("no-params").element.text should be ("Howdy!")
   }
 
-  "The 1-arg value" should "be 'Goodbye, Cruel World!'" in {
+  "The 1-arg value on the i18n-1name page" should "be 'Goodbye, Cruel World!'" in {
     id("params").element.text should be ("Goodbye, Cruel World!")
   }
 
@@ -20,11 +20,31 @@ class I18nSpecs extends BaseSpec{
     eventually { pageTitle should be ("App: i18n - Non-English") }
   }
 
-  "The no-arg value" should "be the correct Chinese" in {
+  "The no-arg value on the i18n-non-english page" should "be the correct Chinese" in {
     id("no-params").element.text should be ("\u4F60\u597D")
   }
 
-  "The 1-arg value" should "be the correct Hebrew" in {
+  "The 1-arg value on the i18n-non-english page" should "be the correct Hebrew" in {
     id("params").element.text should be ("!Cruel World ,\u05E9\u05DC\u05D5\u05DD")
   }
+
+
+  "The i18n-2names page" should "load" in {
+    go to s"$index/i18n-2names"
+    eventually { pageTitle should be ("App: i18n - 2 names") }
+  }
+
+  "The no-arg value on the i18n-2names page" should "be 'Howdy!'" in {
+    id("no-params").element.text should be ("Howdy!")
+  }
+
+  "The 1-arg value on the i18n-2names page" should "be 'Goodbye, Cruel World!'" in {
+    id("params").element.text should be ("Goodbye, Cruel World!")
+  }
+
+  "The lift-bundle value on the i18n-2names page" should "be 'Lost Password'" in {
+    id("other").element.text should be ("Lost Password")
+  }
+
+
 }
