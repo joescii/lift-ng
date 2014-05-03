28b60fc022bd8693f96f0295d1e4397a8bdb4bba
Joe Barnes <barnesjd@gmail.com>
2014-05-03 13:17:53 -0500

    Adding version to liftproxy.js file name for browser caching

--- project/LiftNgBuild.scala
+++ project/LiftNgBuild.scala
@@ -7,8 +7,25 @@ object LiftNgBuild extends Build {
 
   val liftEdition = SettingKey[String]("liftEdition", "Lift Edition (short version number to append to artifact name)")
   
+  val copyJs = resourceGenerators in Compile <+= (sourceDirectory, resourceManaged in Compile, version) map { (src, rsrc, ver) =>
+    val jsSrcDir = src / "main" / "js"
+    val jsDstDir = rsrc
+    val jsSrcs = IO.listFiles(jsSrcDir)
+    val srcRelNames = jsSrcs.map(_.getPath.substring(jsSrcDir.getPath.length+1))
+    val withVer = srcRelNames.map(n => n.substring(0, n.length-3)+"-"+ver+".js")
+    val dstRelNames = withVer.map("toserve/net/liftmodules/ng/js/" + _)
+    val jsDsts = dstRelNames.map(new File(jsDstDir, _))
+    (jsSrcs zip jsDsts) map { case (src, dst) => IO.copyFile(src, dst) }
+    jsDsts.toSeq
+  }
+  
+  val liftngSettings = Project.defaultSettings ++ Seq(
+    copyJs
+  )
+  
   lazy val project = Project(
     id = "lift-ng", 
     base = file("."),
-    settings = Project.defaultSettings)
+    settings = liftngSettings
+  )
 }
