8ffc31f549c7fc721770a5eeb44a286b877c2a32
Joe Barnes <barnesjd@gmail.com>
2014-06-22 16:39:04 -0500

    Serving i18n resources restfully

---
+++ src/main/scala/net/liftmodules/ng/I18n.scala
@@ -0,0 +1,42 @@
+package net.liftmodules.ng
+
+import net.liftweb.http.{JavaScriptResponse, LiftRules, DispatchSnippet, S}
+import net.liftweb.http.js.JE._
+import net.liftweb.http.js.JsCmds._
+import com.joescii.j2jsi18n.JsResourceBundle
+import scala.xml.NodeSeq
+import net.liftweb.http.rest.RestHelper
+
+object AngularI18n extends DispatchSnippet {
+  /** Implementation of dispatch to allow us to add ourselves as a snippet */
+  override def dispatch = {
+    case _ => { _ => render }
+  }
+
+  def render:NodeSeq = {
+    val fromName  = S.attr("name").map(_.toString).toList
+    val fromNames = S.attr("names").map(_.toString.split(',')).toList.flatten
+    val names = fromName ++ fromNames
+    Script(toModule(names))
+  }
+
+  def toModule(names:List[String]) = {
+    val rsrcs = LiftRules.resourceNames.zip(S.resourceBundles).filter{ case (name, b) => names.contains(name) }.toMap
+    val moduleDeclaration = Call("angular.module", "i18n", JsArray())
+    rsrcs.foldLeft(moduleDeclaration){ case (module, (name, bundle)) =>
+      val jsb = new JsResourceBundle(bundle)
+      Call(JsVar(module.toJsCmd, "factory").toJsCmd, name, AnonFunc(JsReturn(JsRaw(jsb.toJs))))
+    }
+  }
+}
+
+object AngularI18nRest extends RestHelper {
+  def init() {
+    LiftRules.statelessDispatch.append(AngularI18nRest)
+  }
+
+  serve {
+    case "lift-ng" :: "i18n" :: name :: Nil Get _ =>
+      new JavaScriptResponse(AngularI18n.toModule(List(name)), Nil, Nil, 200)
+  }
+}
