86419e3f88eee90a96dda33a8cdbb79f6eff6e4e
Joe Barnes <barnesjd@gmail.com>
2014-06-23 09:27:56 -0500

    Added MD5 digest to i18n urls to ensure the browser caches appropriately

--- src/main/scala/net/liftmodules/ng/I18n.scala
+++ src/main/scala/net/liftmodules/ng/I18n.scala
@@ -6,6 +6,8 @@ import net.liftweb.http.js.JsCmds._
 import com.joescii.j2jsi18n.JsResourceBundle
 import scala.xml.NodeSeq
 import net.liftweb.http.rest.RestHelper
+import java.security.MessageDigest
+import java.math.BigInteger
 
 object AngularI18n extends DispatchSnippet {
   /** Implementation of dispatch to allow us to add ourselves as a snippet */
@@ -17,7 +19,7 @@ object AngularI18n extends DispatchSnippet {
     val fromName  = S.attr("name").map(_.toString).toList
     val fromNames = S.attr("names").map(_.toString.split(',')).toList.flatten
     val names = fromName ++ fromNames
-    val src = "lift-ng/i18n?"+(names.map("name="+_).mkString("&"))
+    val src = "lift-ng/i18n?"+(names.map(urlParam).mkString("&"))
     <script src={src}></script>
   }
 
@@ -28,6 +30,19 @@ object AngularI18n extends DispatchSnippet {
       val jsb = new JsResourceBundle(bundle)
       Call(JsVar(module.toJsCmd, "factory").toJsCmd, name, AnonFunc(JsReturn(JsRaw(jsb.toJs))))
     }
+  }
+
+  def digest(str:String):String = {
+    val md5 = MessageDigest.getInstance("MD5")
+    val digest = md5.digest(str.getBytes("UTF-8"))
+    new BigInteger(1, digest).toString(16)
+  }
+
+  def urlParam(name:String):String = {
+    val module = toModule(List(name)).toString()
+    println(module)
+    val sum = digest(module)
+    "name="+name+"&sum="+sum
   }
 }
 
