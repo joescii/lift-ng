0c72738ac1a906ed9bf0aa859a6a6c0825d7de68
Joe Barnes <barnesjd@gmail.com>
2014-07-21 07:15:35 -0500

    Got the client pushing its model back to the server

--- src/main/scala/net/liftmodules/ng/BindingActor.scala
+++ src/main/scala/net/liftmodules/ng/BindingActor.scala
@@ -4,9 +4,10 @@ import Angular.NgModel
 import net.liftweb.json.{ DefaultFormats, parse }
 import net.liftweb.json.Serialization._
 import net.liftweb.json.JsonAST._
-import net.liftweb.http.js.JE.JsVar
+import net.liftweb.http.js.JE._
 import js.JsonDeltaFuncs._
 import net.liftweb.http.js.JsCmds._
+import net.liftweb.http.SHtml._
 
 trait BindingActor extends AngularActor {
   /** The client `\$scope` element to bind to */
@@ -15,7 +16,15 @@ trait BindingActor extends AngularActor {
   var stateModel:Any = null
   var stateJson:JValue = JNull
 
-  override def render = nodesToRender ++ Script(buildCmd(root = false, SetExp(JsVar("s."+bindTo), stateJson)))
+  override def render = nodesToRender ++ Script(buildCmd(root = false,
+    SetExp(JsVar("s."+bindTo), stateJson) &
+    Call("s.$watch", JString(bindTo), AnonFunc("updated",
+      Call("console.log", JsVar("updated")) &
+      ajaxCall(JsVar("updated"), s => {
+        logger.debug(s)
+        Noop
+    })))
+  ))
 
   def toJValue(m:Any):JValue = m match {
     case m:NgModel  => parse(write(m))
