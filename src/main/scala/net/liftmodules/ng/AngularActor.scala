5b6436c2c87cf66f9c42a255d282f3d01fdc92c1
Joe Barnes <barnesjd@gmail.com>
2014-07-21 08:08:48 -0500

    Got the dequeuing of messages issue resolved

--- src/main/scala/net/liftmodules/ng/AngularActor.scala
+++ src/main/scala/net/liftmodules/ng/AngularActor.scala
@@ -34,29 +34,29 @@ trait AngularActor extends CometActor with Loggable {
 
   private val varElement = JsCrVar("e", Call("angular.element", Call("document.querySelector", JString("#"+id))))// "var s=angular.element(document.querySelector('#"+id+"')).scope();"
   /** Variable assignment for \$scope */
-  private val varScope = JsCrVar("s", Call("e.scope"))
+  protected val scopeExp = "e.scope()"
   /** Variable assignment for \$rootScope */
-  private val varRoot  = JsCrVar("r", JsRaw("(typeof s==='undefined')?void 0:s.$root"))// "var r=(typeof s==='undefined')?void 0:s.$root;"
+  protected val rootExp  = "((typeof "+scopeExp+"==='undefined')?void 0:"+scopeExp+".$root)"
+
+  def targetExp(root:Boolean) = (if(root) rootExp else scopeExp)
 
   /** Sends any of our commands with all of the early-arrival retry mechanism packaged up */
   protected def buildCmd(root:Boolean, f:JsCmd):JsCmd = {
-    val scopeVar = if(root) "r" else "s"
-    val vars = varElement & varScope & (if(root) varRoot else Noop)
-    val ready = JsCrVar("t", AnonFunc(JsReturn(JsRaw("typeof " + scopeVar + "!=='undefined'"))))
-    val fn = JsCrVar("f", AnonFunc(Call(scopeVar+".$apply", AnonFunc(f))))
+    val ready = JsCrVar("t", AnonFunc(JsReturn(JsRaw("typeof e.scope()!=='undefined'"))))
+    val fn = JsCrVar("f", AnonFunc(Call("("+targetExp(root)+").$apply", AnonFunc(f))))
     val dequeue = "var d=function(){" +
       "if(net_liftmodules_ng_q[0].t()){"+
         "for(i=0;i<net_liftmodules_ng_q.length;i++){" +
            "net_liftmodules_ng_q[i].f();"+
           "}"+
         "net_liftmodules_ng_q=void 0;"+
-      "}else{"+
+      "}else{console.log('resetting timeout');"+
         "setTimeout(function(){d();},"+interval+");"+
       "}"+
     "};"
     val enqueue = "if(typeof net_liftmodules_ng_q==='undefined'){net_liftmodules_ng_q=[];setTimeout(function(){d();},"+interval+");}" +
       "net_liftmodules_ng_q.push({t:t,f:f});"
-    val cmds = vars & ready & fn & JsRaw(dequeue+"if(typeof net_liftmodules_ng_q==='undefined'&&t()){f();}else{"+enqueue+"}")
+    val cmds = varElement & ready & fn & JsRaw(dequeue+"if(typeof net_liftmodules_ng_q==='undefined'&&t()){f();}else{"+enqueue+"}")
     logger.debug(cmds)
     cmds
   }
@@ -71,16 +71,15 @@ trait AngularActor extends CometActor with Loggable {
     def assign(field:String, obj:AnyRef):Unit = partialUpdate(assignCmd(field, obj))
 
     protected def root:Boolean
-    private def scopeVar = if(root) "r" else "s"
 
     /** Sends an event command, i.e. broadcast or emit */
     private def eventCmd(method:String, event:String, obj:AnyRef):JsCmd = {
-      buildCmd(root, JsRaw(scopeVar+".$"+method+"('"+event+"',"+stringify(obj)+")"))
+      buildCmd(root, JsRaw(targetExp(root)+".$"+method+"('"+event+"',"+stringify(obj)+")"))
     }
 
     /** Sends an assignment command */
     private def assignCmd(field:String, obj:AnyRef):JsCmd = {
-      buildCmd(root, JsRaw(scopeVar+"."+field+"="+stringify(obj)))
+      buildCmd(root, JsRaw(targetExp(root)+"."+field+"="+stringify(obj)))
     }
   }
 
