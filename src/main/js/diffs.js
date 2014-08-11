7ca157a5074c693bbc386b2fde0b9691bd2da36f
Joe Barnes <barnesjd@gmail.com>
2014-08-11 16:24:00 -0500

    Updated js diff to add entirely missing keys

--- src/main/js/diffs.js
+++ src/main/js/diffs.js
@@ -1,5 +1,60 @@
+
+
 var net_liftmodules_ng = net_liftmodules_ng || {};
 
 ;(function(){
-  net_liftmodules_ng.diff = function(){};
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+  if (true) {
+    Object.keys = (function () {
+      'use strict';
+      var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+        dontEnums = [
+          'toString',
+          'toLocaleString',
+          'valueOf',
+          'hasOwnProperty',
+          'isPrototypeOf',
+          'propertyIsEnumerable',
+          'constructor'
+        ],
+        dontEnumsLength = dontEnums.length;
+
+      return function (obj) {
+        if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+          throw new TypeError('Object.keys called on non-object');
+        }
+
+        var result = [], prop, i;
+
+        for (prop in obj) {
+          if (hasOwnProperty.call(obj, prop)) {
+            result.push(prop);
+          }
+        }
+
+        if (hasDontEnumBug) {
+          for (i = 0; i < dontEnumsLength; i++) {
+            if (hasOwnProperty.call(obj, dontEnums[i])) {
+              result.push(dontEnums[i]);
+            }
+          }
+        }
+        return result;
+      };
+    }());
+  }
+
+  net_liftmodules_ng.diff = function(a, b){
+    var add = {};
+    var sub = {};
+    var keys = Object.keys(a);
+
+    for(var i in keys) {
+      var k = keys[i];
+      if(a[k] !== b[k]) add[k] = a[k];
+    }
+
+    return {add:add, sub:sub};
+  };
 })();
--- src/test/js/specs/Diff.spec.js
+++ src/test/js/specs/Diff.spec.js
@@ -1,8 +1,24 @@
 describe('lift-ng', function(){
 
   describe('json diff', function(){
-    it('bootstrap', function() {
-      expect(net_liftmodules_ng.diff).toBeDefined();
+    it('should produce empty add/sub for the same object', function() {
+      var val1 = {str: "a string"};
+      var val2 = {str: "a string"};
+      var diff = net_liftmodules_ng.diff(val1, val2);
+      var expected = {add:{}, sub:{}};
+
+      expect(diff).toEqual(expected);
     });
+
+    it('should produce add for adding a string field', function() {
+      var val1 = {str: "a string"};
+      var val2 = {};
+      var diff = net_liftmodules_ng.diff(val1, val2);
+      var expected = {add:{str: "a string"}, sub:{}};
+
+      expect(diff).toEqual(expected);
+    });
+
+
   });
 });
