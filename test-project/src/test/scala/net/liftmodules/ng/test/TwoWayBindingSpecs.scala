29849bab1ebc2563c2f56e63f3a9a9024db31433
Joe Barnes <barnesjd@gmail.com>
2014-09-28 13:02:55 -0500

    Added automated test for two-way binding

--- test-project/src/test/scala/net/liftmodules/ng/test/TwoWayBindingSpecs.scala
+++ test-project/src/test/scala/net/liftmodules/ng/test/TwoWayBindingSpecs.scala
@@ -6,4 +6,13 @@ class TwoWayBindingSpecs extends BaseSpec {
     eventually { pageTitle should be ("App: 2-way Binding") }
   }
 
+  "The server" should "increment the count" in {
+    eventually(textField("counter").value should be ("0"))
+    textField("counter").value = "1"
+    eventually(textField("counter").value should be ("2"))
+    textField("counter").value = "12"
+    eventually(textField("counter").value should be ("13"))
+    textField("counter").value = "99"
+    eventually(textField("counter").value should be ("100"))
+  }
 }
