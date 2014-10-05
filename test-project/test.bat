b5ff3f6c4716e28f3537d51f93e7f060df401a4f
Joe Barnes <barnesjd@gmail.com>
2014-10-05 17:11:14 -0500

    Reversing the order of versions under test to coincide with publishing, so they can be run concurrently

--- test-project/test.bat
+++ test-project/test.bat
@@ -5,11 +5,7 @@ set SBT=C:\sbt\bin\sbt
 set tasks=clean update test
 set el=0
 
-call %SBT% -Dlift.version=2.5.1 -Dscala.version=2.10.4 %tasks%
-set el=%errorlevel%
-if %el% NEQ 0 goto EXIT
-
-call %SBT% -Dlift.version=2.6-RC1 -Dscala.version=2.10.4 %tasks%
+call %SBT% -Dlift.version=3.0-SNAPSHOT -Dscala.version=2.11.2 %tasks%
 set el=%errorlevel%
 if %el% NEQ 0 goto EXIT
 
@@ -17,7 +13,11 @@ call %SBT% -Dlift.version=2.6-RC1 -Dscala.version=2.11.2 %tasks%
 set el=%errorlevel%
 if %el% NEQ 0 goto EXIT
 
-call %SBT% -Dlift.version=3.0-SNAPSHOT -Dscala.version=2.11.2 %tasks%
+call %SBT% -Dlift.version=2.6-RC1 -Dscala.version=2.10.4 %tasks%
+set el=%errorlevel%
+if %el% NEQ 0 goto EXIT
+
+call %SBT% -Dlift.version=2.5.1 -Dscala.version=2.10.4 %tasks%
 set el=%errorlevel%
 if %el% NEQ 0 goto EXIT
 
