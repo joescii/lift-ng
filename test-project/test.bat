e5f00a7bb7a67dd1ef3543c7d18a63c01d18ac1d
Joe Barnes <barnesjd@gmail.com>
2014-10-12 10:31:04 -0500

    Adding version messages to the end of tests so we'll know which one last ran

--- test-project/test.bat
+++ test-project/test.bat
@@ -7,18 +7,22 @@ set el=0
 
 call %SBT% -Dlift.version=3.0-SNAPSHOT -Dscala.version=2.11.2 %tasks%
 set el=%errorlevel%
+echo 3.0/2.11 complete
 if %el% NEQ 0 goto EXIT
 
 call %SBT% -Dlift.version=2.6-RC1 -Dscala.version=2.11.2 %tasks%
 set el=%errorlevel%
+echo 2.6/2.11 complete
 if %el% NEQ 0 goto EXIT
 
 call %SBT% -Dlift.version=2.6-RC1 -Dscala.version=2.10.4 %tasks%
 set el=%errorlevel%
+echo 2.6/2.10 complete
 if %el% NEQ 0 goto EXIT
 
 call %SBT% -Dlift.version=2.5.1 -Dscala.version=2.10.4 %tasks%
 set el=%errorlevel%
+echo 2.5.1/2.10 complete
 if %el% NEQ 0 goto EXIT
 
 :EXIT
