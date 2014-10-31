ea4d7a4fcb6f1d3d6f8e22694d15805e4927e241
Joe Barnes <barnesjd@gmail.com>
2014-10-30 20:29:22 -0500

    Added spec for embedded-futures

---
+++ test-project/src/test/scala/net/liftmodules/ng/test/EmbeddedFuturesSpec.scala
@@ -0,0 +1,19 @@
+package net.liftmodules.ng.test
+
+class EmbeddedFuturesSpec extends BaseSpec {
+  "The futures page" should "load" in {
+    initialize("embedded-futures")
+  }
+
+  "The object" should "load all its stuff" in {
+    click on "go-button"
+    eventually {
+      id("resolved-output").element.text should be ("resolved")
+      id("failed-output").element.text should be ("failed")
+      id("string-output").element.text should be ("future")
+      id("object-str-output").element.text should be ("string")
+      id("object-num-output").element.text should be ("42")
+    }
+  }
+
+}
