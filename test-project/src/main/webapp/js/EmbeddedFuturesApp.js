fa52fc179d8b34515b4917c65db2ab6b4c6869f1
Joe Barnes <barnesjd@gmail.com>
2014-11-01 11:43:12 -0500

    Bumping test project back to angular 1.2.x to play with the promise unwrapping that has been removed in 1.3.x

--- test-project/src/main/webapp/js/EmbeddedFuturesApp.js
+++ test-project/src/main/webapp/js/EmbeddedFuturesApp.js
@@ -1,9 +1,11 @@
 angular.module('EmbeddedFuturesApp', ['EmbeddedFutures'])
+.config(['$parseProvider', function($parseProvider){ $parseProvider.unwrapPromises(true) }])
 .controller('EmbeddedFuturesController', ['$scope', 'embeddedFutureServices', function($scope, svc) {
   $scope.obj = {};
 
   $scope.click = function() {
     svc.fetch().then(function(obj){
+      console.log(obj);
       $scope.obj = obj;
     });
   };
