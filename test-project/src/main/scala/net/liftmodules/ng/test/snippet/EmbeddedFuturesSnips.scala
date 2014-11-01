c4032139b2e1ef5488598c928868a95dc462d291
Joe Barnes <barnesjd@gmail.com>
2014-11-01 11:02:39 -0500

    Re-enabled EmbeddedFuturesSpec and began filling in the services

--- test-project/src/main/scala/net/liftmodules/ng/test/snippet/EmbeddedFuturesSnips.scala
+++ test-project/src/main/scala/net/liftmodules/ng/test/snippet/EmbeddedFuturesSnips.scala
@@ -2,12 +2,29 @@ package net.liftmodules.ng
 package test.snippet
 
 import Angular._
+import net.liftweb.actor.LAFuture
+import net.liftmodules.ng.test.model.Test2Obj
+import net.liftweb.common.{Full, Box, Empty}
+
+case class EmbeddedFutures(
+  resolved: LAFuture[Box[String]]
+//  failed: LAFuture[String],
+//  string: LAFuture[String],
+//  obj: LAFuture[Test2Obj]
+)
 
 object EmbeddedFuturesSnips {
   def services = renderIfNotAlreadyDefined(
     angular.module("EmbeddedFutures")
       .factory("embeddedFutureServices", jsObjFactory()
-        // TODO
+        .future("fetch", {
+          val f = new LAFuture[Box[EmbeddedFutures]]
+          val resolved = new LAFuture[Box[String]]
+          val model = EmbeddedFutures(resolved)
+          f.satisfy(Full(model))
+          resolved.satisfy(Full("resolved"))
+          f
+        })
       )
   )
 }
--- test-project/src/test/scala/net/liftmodules/ng/test/EmbeddedFuturesSpec.scala
+++ test-project/src/test/scala/net/liftmodules/ng/test/EmbeddedFuturesSpec.scala
@@ -6,26 +6,26 @@ class EmbeddedFuturesSpec extends BaseSpec {
     click on "go-button"
   }
 
-  "The future field that resolves before sending" should "be set to 'resolved'" ignore {
+  "The future field that resolves before sending" should "be set to 'resolved'" in {
     eventually { id("resolved-output").element.text should be ("resolved") }
   }
 
-  "The future field that fails" should "be set to 'failed'" ignore {
+  "The future field that fails" should "be set to 'failed'" in {
     eventually { id("failed-output").element.text should be ("failed") }
   }
 
-  "The future field that contains a string" should "be set to 'future'" ignore {
+  "The future field that contains a string" should "be set to 'future'" in {
     eventually { id("string-output").element.text should be ("future") }
   }
 
-  "The future field that contains an object" should "set its two fields" ignore {
+  "The future field that contains an object" should "set its two fields" in {
     eventually {
       id("object-str-output").element.text should be ("string")
       id("object-num-output").element.text should be ("42")
     }
   }
 
-  "The future field that contains an object with its own embedded futures" should "set its num and str fields" ignore {
+  "The future field that contains an object with its own embedded futures" should "set its num and str fields" in {
     eventually {
       id("object-future-str-output").element.text should be ("string")
       id("object-future-num-output").element.text should be ("43")
@@ -33,14 +33,14 @@ class EmbeddedFuturesSpec extends BaseSpec {
     }
   }
 
-  "The future field that contains an object with its own embedded futures" should "set the two fields from the embedded future[object]" ignore {
+  "The future field that contains an object with its own embedded futures" should "set the two fields from the embedded future[object]" in {
     eventually {
       id("object-future-obj-str-output").element.text should be ("string")
       id("object-future-obj-num-output").element.text should be ("44")
     }
   }
 
-  "The future field that contains an object with its own embedded futures" should "set the two fields from the embedded array[future]" ignore {
+  "The future field that contains an object with its own embedded futures" should "set the two fields from the embedded array[future]" in {
     eventually {
       id("object-future-obj-arr1-output").element.text should be ("arr[0]")
       id("object-future-obj-arr2-output").element.text should be ("arr[1]")
