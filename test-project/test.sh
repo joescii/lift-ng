8fda72be54db02d7c94145bad0996f721d44af15
Joe Barnes <barnesjd@gmail.com>
2014-11-05 06:34:41 -0600

    Updating our Lift 3.0 dependency from SNAPSHOT to M2 to improve dependency resolution time

--- test-project/test.sh
+++ test-project/test.sh
@@ -31,7 +31,7 @@ run_tests_rec () {
   return $status
 }
 
-run_tests "3.0-SNAPSHOT" "2.11.2"
+run_tests "3.0-M2" "2.11.2"
 run_tests "2.6-RC1" "2.11.2"
 run_tests "2.6-RC1" "2.10.4"
 run_tests "2.5.1" "2.10.4"
