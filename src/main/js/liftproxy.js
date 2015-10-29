bd9fa0727a4be84241c8b91f7b37b665c08d144f
Joe Barnes <barnesjd@gmail.com>
2015-10-29 12:33:20 -0500

    Only initializing comet and ajax hooks if those objects are defined, since lift inserts them only if necessary. Finally bit the bullet to make things more professional by wrapping the junk into an anonymous function and all that jazz

--- src/main/js/liftproxy.js
+++ src/main/js/liftproxy.js
@@ -78,7 +78,7 @@ angular
     }
   }])
   .service("liftProxy", ["$rootScope", "$q", "plumbing", function ($rootScope, $q, plumbing) {
-    net_liftmodules_ng.init();
+    window.net_liftmodules_ng.init();
 
     var onServerCommError = function(count, which, req) {
       $rootScope.$emit("net_liftmodules_ng.serverCommError", count, which, req);
@@ -88,7 +88,7 @@ angular
       $rootScope.$emit("net_liftmodules_ng.serverCommErrorClear", which);
     };
 
-    net_liftmodules_ng.onServerCommErrorClearCallbacks.push(onServerCommErrorClear);
+    window.net_liftmodules_ng.onServerCommErrorClearCallbacks.push(onServerCommErrorClear);
 
     var onCometError = function(count) {
       $rootScope.$apply(function(){
@@ -96,7 +96,7 @@ angular
       });
     };
 
-    net_liftmodules_ng.onServerCommErrorCallbacks.push(onCometError);
+    window.net_liftmodules_ng.onServerCommErrorCallbacks.push(onCometError);
 
     var onErrorFor = function(req) { return function(count) {
       onServerCommError(count, "ajax", req);
@@ -112,7 +112,7 @@ angular
     }};
 
     var requestFor;
-    if(net_liftmodules_ng.enhancedAjax) requestFor = toEnhancedReq;
+    if(window.net_liftmodules_ng.enhancedAjax) requestFor = toEnhancedReq;
     else requestFor = toData;
 
     var svc = {
@@ -140,7 +140,7 @@ angular
           defer.reject("net.liftmodules.ng.Angular.ajaxErrorRetryExceeded");
         })};
 
-        net_liftmodules_ng.ajax(req, onSuccess, onFailure, "json");
+        window.net_liftmodules_ng.ajax(req, onSuccess, onFailure, "json");
 
         return defer.promise;
       }
@@ -150,79 +150,92 @@ angular
   }
 ]);
 
-var net_liftmodules_ng = net_liftmodules_ng || {};
-// Careful to only init once, since it's possible to have multiple angular apps in one page
-net_liftmodules_ng.isInitialized = false;
-// Using an array of callbacks to handle case of multiple angular apps
-net_liftmodules_ng.onServerCommErrorCallbacks = [];
-net_liftmodules_ng.onServerCommErrorClearCallbacks = [];
-net_liftmodules_ng.init = function() { if(net_liftmodules_ng.enhancedAjax) { // Remove this condition once we can support Lift 3.x
-  if(!net_liftmodules_ng.isInitialized) {
-    var serverCommErrorCount = 0;
-    var clearServerCommError = function(which) {
-      if(serverCommErrorCount != 0) {
-        serverCommErrorCount = 0;
-        for (var i = 0; i < net_liftmodules_ng.onServerCommErrorClearCallbacks.length; i++)
-          net_liftmodules_ng.onServerCommErrorClearCallbacks[i](which);
-      }
-    };
-
-    // We've passed {data, when} to the ajax lift machinery, so we need to pull the data part back out.
-    var onlyData = function(req) {
-      // This check prevents us from screwing up any non-lift-ng ajax calls someone could possibly be making.
-      if(typeof req === "object") return req.data;
-      else return req;
-    };
-
-    var ajaxFailureWrapper = function(req, onFailure) { return function() {
-      // Note that we _could_ count all failures. However, in the unlikely event that someone is doing ajax
-      // outside of lift-ng, failures can also be regular ol' Exceptions in business code which are not any
-      // indication of our ability to communicate with the server via lift-ng.
-      if (typeof req === "object" && typeof req.onError === "function")
-        req.onError(++serverCommErrorCount);
-      onFailure.apply(this, arguments); // We know lift always passes a failure cb function
-    }};
-
-    var ajaxSuccessWrapper = function(onSuccess) { return function() {
-      // Although we are not counting non-lift-ng failures, we will clear any time we successfully communicate with
-      // the server for any reason.
-      clearServerCommError("ajax");
-      onSuccess.apply(this, arguments); // We know lift always passes a success cb function
-    }};
-
-    // Wrap the json call with our hooks in place
-    var origAjax = liftAjax.lift_actualJSONCall;
-    liftAjax.lift_actualJSONCall = function(req, onSuccess, onFailure) {
-      return origAjax(onlyData(req), ajaxSuccessWrapper(onSuccess), ajaxFailureWrapper(req, onFailure));
-    };
-
-    // Override the sort function if we should retry ajax in order.
-    if(net_liftmodules_ng.retryAjaxInOrder) {
-      liftAjax.lift_ajaxQueueSort = function() {
-        liftAjax.lift_ajaxQueue.sort(function (a, b) {
-          // If both items are one of our doctored-up requests, grab our 'when' which is the original request time.
-          if(typeof a.theData === "object" && a.theData.when && typeof b.theData === "object" && b.theData.when)
-            return a.theData.when - b.theData.when;
-          else // Not our stuff, so let's not screw around with the original order logic.
-            return a.when - b.when;
-        });
+(function(){
+  var net_liftmodules_ng = window.net_liftmodules_ng || {};
+  // Careful to only init once, since it's possible to have multiple angular apps in one page
+  net_liftmodules_ng.isInitialized = false;
+  // Using an array of callbacks to handle case of multiple angular apps
+  net_liftmodules_ng.onServerCommErrorCallbacks = [];
+  net_liftmodules_ng.onServerCommErrorClearCallbacks = [];
+  net_liftmodules_ng.init = function() { if(net_liftmodules_ng.enhancedAjax) { // Remove this condition once we can support Lift 3.x
+    if(!net_liftmodules_ng.isInitialized) {
+      var serverCommErrorCount = 0;
+      var clearServerCommError = function (which) {
+        if (serverCommErrorCount != 0) {
+          serverCommErrorCount = 0;
+          for (var i = 0; i < net_liftmodules_ng.onServerCommErrorClearCallbacks.length; i++)
+            net_liftmodules_ng.onServerCommErrorClearCallbacks[i](which);
+        }
       };
+
+      if (liftAjax) {
+        // We've passed {data, when} to the ajax lift machinery, so we need to pull the data part back out.
+        var onlyData = function (req) {
+          // This check prevents us from screwing up any non-lift-ng ajax calls someone could possibly be making.
+          if (typeof req === "object") return req.data;
+          else return req;
+        };
+
+        var ajaxFailureWrapper = function (req, onFailure) {
+          return function () {
+            // Note that we _could_ count all failures. However, in the unlikely event that someone is doing ajax
+            // outside of lift-ng, failures can also be regular ol' Exceptions in business code which are not any
+            // indication of our ability to communicate with the server via lift-ng.
+            if (typeof req === "object" && typeof req.onError === "function")
+              req.onError(++serverCommErrorCount);
+            onFailure.apply(this, arguments); // We know lift always passes a failure cb function
+          }
+        };
+
+        var ajaxSuccessWrapper = function (onSuccess) {
+          return function () {
+            // Although we are not counting non-lift-ng failures, we will clear any time we successfully communicate with
+            // the server for any reason.
+            clearServerCommError("ajax");
+            onSuccess.apply(this, arguments); // We know lift always passes a success cb function
+          }
+        };
+
+        // Wrap the json call with our hooks in place
+        var origAjax = window.liftAjax.lift_actualJSONCall;
+        window.liftAjax.lift_actualJSONCall = function (req, onSuccess, onFailure) {
+          return origAjax(onlyData(req), ajaxSuccessWrapper(onSuccess), ajaxFailureWrapper(req, onFailure));
+        };
+
+        // Override the sort function if we should retry ajax in order.
+        if (net_liftmodules_ng.retryAjaxInOrder) {
+          window.liftAjax.lift_ajaxQueueSort = function () {
+            window.liftAjax.lift_ajaxQueue.sort(function (a, b) {
+              // If both items are one of our doctored-up requests, grab our 'when' which is the original request time.
+              if (typeof a.theData === "object" && a.theData.when && typeof b.theData === "object" && b.theData.when)
+                return a.theData.when - b.theData.when;
+              else // Not our stuff, so let's not screw around with the original order logic.
+                return a.when - b.when;
+            });
+          };
+        }
+      }
+
+      if (window.liftComet) {
+        var origCometOnFailure = window.liftComet.lift_handlerFailureFunc;
+        window.liftComet.lift_handlerFailureFunc = function () {
+          serverCommErrorCount++;
+          for (var i = 0; i < net_liftmodules_ng.onServerCommErrorCallbacks.length; i++)
+            net_liftmodules_ng.onServerCommErrorCallbacks[i](serverCommErrorCount);
+          origCometOnFailure.apply(this, arguments);
+        };
+
+        var origCometOnSuccess = window.liftComet.lift_handlerSuccessFunc;
+        window.liftComet.lift_handlerSuccessFunc = function () {
+          clearServerCommError("comet");
+          origCometOnSuccess.apply(this, arguments);
+        };
+
+      }
+
+      net_liftmodules_ng.isInitialized = true;
     }
+  }};
 
-    var origCometOnFailure = liftComet.lift_handlerFailureFunc;
-    liftComet.lift_handlerFailureFunc = function() {
-      serverCommErrorCount++;
-      for(var i = 0; i<net_liftmodules_ng.onServerCommErrorCallbacks.length; i++)
-        net_liftmodules_ng.onServerCommErrorCallbacks[i](serverCommErrorCount);
-      origCometOnFailure.apply(this, arguments);
-    };
-
-    var origCometOnSuccess = liftComet.lift_handlerSuccessFunc;
-    liftComet.lift_handlerSuccessFunc = function() {
-      clearServerCommError("comet");
-      origCometOnSuccess.apply(this, arguments);
-    };
-
-    net_liftmodules_ng.isInitialized = true;
-  }
-}};
+  window.net_liftmodules_ng = net_liftmodules_ng;
+}).call(this);
