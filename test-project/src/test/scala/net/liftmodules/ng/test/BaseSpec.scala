adce80a637a4978ac7d01ebbec225ec1dca0565b
Joe Barnes <barnesjd@gmail.com>
2017-04-03 12:19:07 -0500

    Adding patience to make testing a little less flaky

--- test-project/src/test/scala/net/liftmodules/ng/test/BaseSpec.scala
+++ test-project/src/test/scala/net/liftmodules/ng/test/BaseSpec.scala
@@ -8,7 +8,7 @@ import selenium._
 import org.openqa.selenium._
 import firefox.FirefoxDriver
 //import safari.SafariDriver
-//import chrome.ChromeDriver
+import chrome.ChromeDriver
 //import ie.InternetExplorerDriver
 
 trait BaseSpec extends FlatSpecLike with ShouldMatchers with Eventually with WebBrowser with BeforeAndAfterAll {
@@ -16,11 +16,11 @@ trait BaseSpec extends FlatSpecLike with ShouldMatchers with Eventually with Web
 
   val index    = "http://localhost:8080"
 
-  implicit override val patienceConfig = PatienceConfig(timeout = scaled(Span(2, Seconds)), interval = scaled(Span(100, Millis)))
+  implicit override val patienceConfig = PatienceConfig(timeout = scaled(Span(5, Seconds)), interval = scaled(Span(500, Millis)))
 
   implicit val webDriver: WebDriver = Option(System.getProperty("net.liftmodules.ng.test.browser")) match {
     case Some("firefox") => new FirefoxDriver() // Currently only this one will work due to need for drivers of the others.
-    //    case Some("chrome") => new ChromeDriver()
+    case Some("chrome") => new ChromeDriver()
     //    case Some("ie32") => new InternetExplorerDriver()
     //    case Some("ie64") => new InternetExplorerDriver()
     //    case Some("safari") => new SafariDriver()
