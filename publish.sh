89536657ca2f36982a49413e91c9be35b611647b
Joe Barnes <barnesjd@gmail.com>
2017-04-03 12:20:45 -0500

    Replacing the publish commands with a publish script because the commands kept freezing sbt up. Also dropped pre-2.10 support. Doesn't play well with Java 8

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PUBLISH=publishLocal
+
+publish() {
+  LIFT_VERSION="set liftVersion in ThisBuild := \"$1\""
+  CROSS_SCALA="set crossScalaVersions := Seq($2)"
+
+  sbt "$LIFT_VERSION" "$CROSS_SCALA" "+ update" "+ test" "+ $PUBLISH"
+}
+
+sbt clean jasmine
+
+publish "3.1.0-M1" '"2.11.8"'
+publish "3.0.1" '"2.11.8"'
+publish "2.6.3" '"2.11.8", "2.10.5"'
+publish "2.5.4" '"2.10.5"'
