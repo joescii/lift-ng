2431ab7d35036ba769a66ef970a55e7e3a61d259
Joe Barnes <joe@joescii.com>
2017-06-19 09:50:33 -0500

    Updating Lift 3.1 to the latest cut (RC1)

--- publish.sh
+++ publish.sh
@@ -15,7 +15,7 @@ publish() {
 
 sbt clean jasmine
 
-publish "3.1.0-M2" '"2.11.8"'
+publish "3.1.0-RC1" '"2.11.8"'
 publish "3.0.1" '"2.11.8"'
 publish "2.6.3" '"2.11.8", "2.10.5"'
 publish "2.5.4" '"2.10.5"'
