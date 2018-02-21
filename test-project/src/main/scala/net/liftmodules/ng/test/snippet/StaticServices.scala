cb56f8a353985f12df100cc82294216f33cc5698
Joe Barnes <joe@joescii.com>
2018-02-21 09:15:40 -0600

    Removed last of the lift-ng deprecation warnings

--- test-project/src/main/scala/net/liftmodules/ng/test/snippet/StaticServices.scala
+++ test-project/src/main/scala/net/liftmodules/ng/test/snippet/StaticServices.scala
@@ -12,9 +12,9 @@ object StaticServices {
 
   def render(xhtml:NodeSeq) = renderIfNotAlreadyDefined(
     angular.module("StaticServices").factory("staticService", jsObjFactory()
-      .string("string", "FromServer1")
+      .valAny("string", "FromServer1")
       .valAny("integer", 42)
-      .json("obj", StringInt("FromServer2", 88))
+      .valAny("obj", StringInt("FromServer2", 88))
     )
   )
 }
